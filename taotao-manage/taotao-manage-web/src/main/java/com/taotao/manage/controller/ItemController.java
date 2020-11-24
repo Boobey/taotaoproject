ac6d32ebe88b8ba966af5cd37a3b0c3ec4825dd7
Infi <2675784852@qq.com>
2020-11-24 09:22:38 +0800

    日志书写实现

--- taotao-manage/taotao-manage-web/src/main/java/com/taotao/manage/controller/ItemController.java
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/manage/controller/ItemController.java
@@ -4,6 +4,8 @@ import com.taotao.manage.pojo.Item;
 import com.taotao.manage.pojo.ItemDesc;
 import com.taotao.manage.service.ItemDescService;
 import com.taotao.manage.service.ItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +19,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ItemController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ItemController.class);
+
     @Autowired
     private ItemService itemService;
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> saveItem(Item item, @RequestParam("desc") String desc) {
         try {
+            if (LOGGER.isDebugEnabled())  {
+                LOGGER.debug("新增商品, item = {}, desc = {}", item, desc);
+            }
+
             if (StringUtils.isEmpty(item.getTitle())) {
                 // 参数有误，400
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
@@ -31,12 +39,20 @@ public class ItemController {
             // 保存商品
             Boolean bool = this.itemService.saveItem(item, desc);
             if (!bool) {
+                if (LOGGER.isInfoEnabled()) {
+                    LOGGER.info("新增商品失败, item = {}", item);
+                }
+
                 // 保存失败
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
             }
+            if (LOGGER.isInfoEnabled()) {
+                LOGGER.info("新增商品成功, itemId = {}", item.getId());
+            }
+
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error("新增商品出错! item = " + item, e);
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
